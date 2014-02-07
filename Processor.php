3ef81606430d1a2c6cca4940c0d216d38814e0e3
Shuhei Tanuma <shuhei.tanuma@gree.co.jp>
2014-02-07 18:33:51 +0900

    rewrite files

--- Processor.php
+++ Processor.php
@@ -5,7 +5,13 @@ use Composer\IO\IOInterface;
 
 class Processor
 {
+    /** @var \Composer\IO\IOInterface $io */
     protected $io;
+
+    protected $messages = array(
+        "project" => "project name",
+        "renderer" => "default renderer",
+    );
 
     public function __construct(IOInterface $io)
     {
@@ -15,11 +21,54 @@ class Processor
     public function perform($configs)
     {
         $config = array_merge(array(
-            "project" => "example"
+            "project" => "example",
+            "renderer" => "smarty"
         ), $configs);
 
         $config = $this->processParams($config, $config);
-        var_dump($config);
+
+        $targets = array(
+            "app/action/Index.php",
+            "app/view/Index.php",
+            "app/Example_ActionClass.php",
+            "app/Example_ActionForm.php",
+            "app/Example_Controller.php",
+            "app/Example_Error.php",
+            "app/Example_UrlHandler.php",
+            "app/Example_ViewClass.php",
+            "etc/example-ini.php",
+            "skel/skel.action.php",
+            "skel/skel.action_cli.php",
+            "skel/skel.action_test.php",
+            "skel/skel.app_object.php",
+            "skel/skel.entry_cli.php",
+            "skel/skel.entry_www.php",
+            "skel/skel.template.tpl",
+            "skel/skel.view.php",
+            "skel/skel.view_test.php",
+            "www/index.php",
+            "www/info.php",
+            "www/xmlrpc.php",
+        );
+
+        $project_class = self::camerize($config['project']);
+        foreach ($targets as $target) {
+            $data = file_get_contents($target);
+            $data = preg_replace("/Example/", $project_class, $data);
+            file_put_contents($target, $data);
+            $name = basename($target);
+
+            if (preg_match("/Example/", $name)) {
+                $name = preg_replace("/Example/", $project_class, $name);
+                $dir = dirname($target);
+                rename($target, $dir . DIRECTORY_SEPARATOR . $name);
+            }
+        }
+    }
+
+    protected static function camerize($name)
+    {
+        return str_replace(' ','', ucwords(str_replace('_', ' ', $name)));
     }
 
     protected function processParams($params, $actualParams)
@@ -27,14 +76,18 @@ class Processor
         if (!$this->io->isInteractive()) {
             return $actualParams;
         }
+        $this->io->write('<comment>Some parameters are missing. Please provide them.</comment>');
 
-        var_dump($params);
         foreach ($params as $key => $value) {
-             $actualParams[$key] = $this->io->ask(
-                 $this->io->ask("<question>%s</question> (<comment>%s</comment>)", $key, $value),
-                 $value
-             );
+            $result = $this->io->ask(
+                sprintf("<question>%s</question> (<comment>%s</comment>): ", $this->messages[$key], $value)
+            );
+
+            if (!empty($result)) {
+                $actualParams[$key] = $result;
+            }
         }
+
         return $actualParams;
     }
 }
--- ScriptHandler.php
+++ ScriptHandler.php
@@ -7,16 +7,10 @@ class ScriptHandler
 {
     public static function buildProject(Event $event)
     {
-        $event->getComposer()->getPackage()->getExtra();
-        var_dump($event->getComposer());
-        var_dump($event->getComposer()->getPackage()->getName());
-        var_dump($event->getComposer()->getPackage()->getTargetDir());
-        var_dump($event->getName());
-        //var_dump($event-)
-
         $processor = new Processor($event->getIO());
         $processor->perform(array(
-            "project" => "example",
+            /* NOTE(chobie): How do I get target install directory? */
+            "project" => basename(getcwd()),
         ));
     }
 }
