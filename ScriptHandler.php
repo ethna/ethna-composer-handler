a003e9f638bac93373959b88ec9d963539874e51
Shuhei Tanuma <shuhei.tanuma@gree.co.jp>
2014-02-07 17:29:46 +0900

    wip

--- ScriptHandler.php
+++ ScriptHandler.php
@@ -8,6 +8,7 @@ class ScriptHandler
     public static function buildProject(Event $event)
     {
         $event->getComposer()->getPackage()->getExtra();
+        var_dump($event->getComposer());
         var_dump($event->getComposer()->getPackage()->getName());
         var_dump($event->getComposer()->getPackage()->getTargetDir());
         var_dump($event->getName());
